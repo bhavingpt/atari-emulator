74806892cd0b8051e3ea7009c39f5424ca873b38
Bhavin <bhavingpt@gmail.com>
2017-04-30 04:53:12 -0500

    parsing all blackjack instructions

--- atari/atari.cpp
+++ atari/atari.cpp
@@ -13,7 +13,7 @@ void next_instruction() {
         table.at(mem[pc])->execute(mem[pc+1], mem[pc+2]);
         pc += table.at(mem[pc])->length();
     } catch (const std::out_of_range& oor) {
-        printf("\nUNKNOWN OPCODE %02x\n\n", mem[pc]);
+        printf("\n%04x: UNKNOWN OPCODE %02x\n\n", pc - 0x1000, mem[pc]);
         running = 0;
     }
 }
--- atari/instructions.cpp
+++ atari/instructions.cpp
@@ -576,8 +576,406 @@ public:
     }
 };
 
+class _30 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 30: conditional jump\n");
+    }
+};
+
+class _E9 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO E9: subtract from acc w carry\n");
+    }
+};
+
+class _3D : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 3D: signed offset and\n");
+        (a == 0) ? set(1) : clr(1);
+        (a >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _E0 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        (x < one) ? clr(0) : set(0);
+        (x == one) ? set(1) : clr(1);
+        (x < one) ? set(7) : clr(7);
+    }
+};
+
+class _06 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 06: misunderstanding shift docs\n");
+    }
+};
+
+class _24 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        ((a & mem[one]) == 0) ? set(1) : clr(1);
+        (mem[one] >> 7) ? set(7) : clr(7);
+        (mem[one] & 0x40) ? set(6) : clr(6);
+    }
+};
+
+class _98 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        a = y;
+        (a == 0) ? set(1) : clr(1);
+        (a >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _B4 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO B4: load with signed offset\n");
+        (y == 0) ? set(1) : clr(1);
+        (y >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _D5 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO D5: signed offset compare\n");
+    }
+};
+
+class _A4 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        y = mem[one];
+        (y == 0) ? set(1) : clr(1);
+        (y >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _BE : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO BE: load with signed offset\n");
+        (x == 0) ? set(1) : clr(1);
+        (x >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _38 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        set(0);
+    }
+};
+
+class _F5 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO F5: subtract operation\n");
+    }
+};
+
+class _00 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 00: break, flag and special behavior\n");
+    }
+};
+
+class _75 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 75: addition operation\n");
+    }
+};
+
+class _C5 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        (a < mem[one]) ? clr(0) : set(0);
+        (a == mem[one]) ? set(1) : clr(1);
+        (a < mem[one]) ? set(7) : clr(7);
+    }
+};
+
+class _08 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 08: push processor to stack\n");
+    }
+};
+
+class _28 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 08: pull processor from stack\n");
+    }
+};
+
+class _19 : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 19: calculating signed offset\n");
+        (a == 0) ? set(1) : clr(1);
+        (a >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _1E : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 1E: don't understand shift docs\n");
+    }
+};
+
+class _01 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 01: signed offset for or\n");
+        (a == 0) ? set(1) : clr(1);
+        (a >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _2E : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 2E: don't understand shift docs\n");
+    }
+};
+
+class _2C : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        ((a & mem[one + (two << 8)]) == 0) ? set(1) : clr(1);
+        (mem[one + (two << 8)] >> 7) ? set(7) : clr(7);
+        (mem[one + (two << 8)] & 0x40) ? set(6) : clr(6);
+    }
+};
+
+class NOP : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {}
+};
+
+class _3E : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 3E: roll with value and flags\n");
+    }
+};
+
+class _0E : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 0E: ASL with values and flags\n");
+    }
+};
+
+class _2A : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 2A: implement ROL A with flags\n");
+    }
+};
+
+class _40 : public Instruction {
+public:
+    int length () { return 1; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 40: return from interrupt\n");
+    }
+};
+
+class _70 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 70: conditional jump\n");
+    }
+};
+
+class _50 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 50: conditional jump\n");
+    }
+};
+
+class _15 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 15: signed offsets\n");
+        (a == 0) ? set(1) : clr(1);
+        (a >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _E4 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        (x < mem[one]) ? clr(0) : set(0);
+        (x == mem[one]) ? set(1) : clr(1);
+        (x < mem[one]) ? set(7) : clr(7);
+    }
+};
+
+class _EE : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO EE: increment memory\n");
+    }
+};
+
+class _AE : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        x = mem[one + (two << 8)];
+        (x == 0) ? set(1) : clr(1);
+        (x >> 7) ? set(7) : clr(7);
+    }
+};
+
+class _6D : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 6d: adder with carry\n");
+    }
+};
+
+class _FE : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO FE: increment write to mem\n");
+    }
+};
+
+class _8C : public Instruction {
+public:
+    int length () { return 3; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 8C: store Y to mem\n");
+    }
+};
+
+class _76 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 76: don't understand shift docs\n");
+    }
+};
+
+class _36 : public Instruction {
+public:
+    int length () { return 2; }
+    void execute (unsigned char one, unsigned char two) {
+        printf("TODO 36: don't understand shift docs\n");
+    }
+};
+
 void initialize_instructions() {
     printf("\n");
+    table.insert(pair<unsigned char, Instruction*>('\x36', new _36()));
+    table.insert(pair<unsigned char, Instruction*>('\x76', new _76()));
+    table.insert(pair<unsigned char, Instruction*>('\x8c', new _8C()));
+    table.insert(pair<unsigned char, Instruction*>('\xfe', new _FE()));
+    table.insert(pair<unsigned char, Instruction*>('\x6d', new _6D()));
+    table.insert(pair<unsigned char, Instruction*>('\xae', new _AE()));
+    table.insert(pair<unsigned char, Instruction*>('\xee', new _EE()));
+    table.insert(pair<unsigned char, Instruction*>('\xe4', new _E4()));
+    table.insert(pair<unsigned char, Instruction*>('\x15', new _15()));
+    table.insert(pair<unsigned char, Instruction*>('\x50', new _50()));
+    table.insert(pair<unsigned char, Instruction*>('\x70', new _70()));
+    table.insert(pair<unsigned char, Instruction*>('\x40', new _40()));
+    table.insert(pair<unsigned char, Instruction*>('\x2a', new _2A()));
+    table.insert(pair<unsigned char, Instruction*>('\x0e', new _0E()));
+    table.insert(pair<unsigned char, Instruction*>('\x3e', new _3E()));
+    table.insert(pair<unsigned char, Instruction*>('\x3c', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x80', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\xf4', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\xc7', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\xf7', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x7f', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x3f', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\xff', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x1c', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x1a', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x12', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x14', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x44', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x42', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x72', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x02', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x77', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x07', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x22', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x17', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x03', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x04', new NOP()));
+    table.insert(pair<unsigned char, Instruction*>('\x2c', new _2C()));
+    table.insert(pair<unsigned char, Instruction*>('\x2e', new _2E()));
+    table.insert(pair<unsigned char, Instruction*>('\x01', new _01()));
+    table.insert(pair<unsigned char, Instruction*>('\x1e', new _1E()));
+    table.insert(pair<unsigned char, Instruction*>('\x19', new _19()));
+    table.insert(pair<unsigned char, Instruction*>('\x28', new _28()));
+    table.insert(pair<unsigned char, Instruction*>('\x08', new _08()));
+    table.insert(pair<unsigned char, Instruction*>('\xc5', new _C5()));
+    table.insert(pair<unsigned char, Instruction*>('\x75', new _75()));
+    table.insert(pair<unsigned char, Instruction*>('\x00', new _00()));
+    table.insert(pair<unsigned char, Instruction*>('\xF5', new _F5()));
+    table.insert(pair<unsigned char, Instruction*>('\x38', new _38()));
+    table.insert(pair<unsigned char, Instruction*>('\xbe', new _BE()));
+    table.insert(pair<unsigned char, Instruction*>('\xa4', new _A4()));
+    table.insert(pair<unsigned char, Instruction*>('\xd5', new _D5()));
+    table.insert(pair<unsigned char, Instruction*>('\xb4', new _B4()));
+    table.insert(pair<unsigned char, Instruction*>('\x98', new _98()));
+    table.insert(pair<unsigned char, Instruction*>('\x24', new _24()));
+    table.insert(pair<unsigned char, Instruction*>('\x06', new _06()));
+    table.insert(pair<unsigned char, Instruction*>('\xe0', new _E0()));
+    table.insert(pair<unsigned char, Instruction*>('\x3d', new _3D()));
+    table.insert(pair<unsigned char, Instruction*>('\xe9', new _E9()));
+    table.insert(pair<unsigned char, Instruction*>('\x30', new _30()));
     table.insert(pair<unsigned char, Instruction*>('\xf8', new _F8()));
     table.insert(pair<unsigned char, Instruction*>('\xc4', new _C4()));
     table.insert(pair<unsigned char, Instruction*>('\xb9', new _B9()));

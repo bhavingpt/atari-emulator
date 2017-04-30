9da6735acec8dc236bdf041077fb39ca73471c79
Bhavin <bhavingpt@gmail.com>
2017-04-29 19:13:35 -0500

    thinking about interpretation implementation

--- nes.cpp
+++ nes.cpp
@@ -8,30 +8,34 @@
 
 uint16_t pc;
 uint8_t a, x, y, sp, p;
-std::unordered_map<unsigned char, int> lengths;
+std::unordered_map<unsigned char, Instruction> table;
 
-void init_lengths();
+class Instruction {
 
-void print_all_instructions() {
-    while (pc < exe[4] * 16384) {
-        int currlen;
-        try {
-            currlen = lengths.at(prg[pc]);
-        } catch (const std::out_of_range& oor) {
-            currlen = 1;
-        }
-        for (int i=0;i < currlen; i++) {
-            printf("%02x ", prg[pc + i]);
-        }
-        printf("\n");
-        pc += currlen;
+}
+
+
+int get_inst_len(unsigned char opcode) {
+
+}
+
+void get_next_instruction() {
+    int currlen = get_inst_len(prg[pc]);
+
+    for (int i=0;i < currlen; i++) {
+        printf("%02x ", prg[pc + i]);
     }
+
+    printf("\n");
+    pc += currlen;
 }
 
 int main(int argc, char** argv) {
     init();
     setup(argv[1]);
 
-    print_all_instructions();
+    while (pc < 50 /*exe[4] * 16384*/) {
+        get_next_instruction();
+    }
 }
 

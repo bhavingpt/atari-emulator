213a284114a36667ef4a3137c532d4fe22a4e8d0
Bhavin <bhavingpt@gmail.com>
2017-05-05 14:10:28 -0500

    video is WORKING - we have static

--- atari/tia.cpp
+++ atari/tia.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <unordered_map>
 #include "reada26.h"
 #include "instructions.h"
@@ -11,25 +12,39 @@ void end_sdl();
 
 char running = 1;
 const int WIDTH = 640;
-const int HEIGHT = 480;
-int frame_depth = 0;
+const int HEIGHT = 384;
+int x_pos = 0;
+int y_pos = 0;
 
+uint32_t* pixels = new uint32_t[640*384];
 SDL_Window* tv = nullptr;
 SDL_Renderer* ren = nullptr;
-SDL_Surface* surf = nullptr;
+SDL_Texture * tex = nullptr;
 
 void color_cycle() {
-    frame_depth++;
+    x_pos++;
+    if (x_pos == 228) {
+        x_pos = 0;
+        y_pos++;
+    }
+
+    if (x_pos > 68 && y_pos > 40 && y_pos < 232) {
+        uint32_t color = rand()%4294967040;
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 4; i++) {
+                pixels[4*(x_pos-68) + i + 640*(2*(y_pos-40) + j)] = color;
+            }
+        }
+    }
 }
 
 void draw_frame() {
-    SDL_Texture *text = SDL_CreateTextureFromSurface(ren, surf);
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, text, NULL, NULL);
+    x_pos = 0;
+    y_pos = 0;
+    SDL_RenderCopy(ren, tex, NULL, NULL);
     SDL_RenderPresent(ren);
-    SDL_Delay(17);
-
-    SDL_DestroyTexture(text);
+    SDL_RenderClear(ren);
+    SDL_UpdateTexture(tex, NULL, pixels, 640 * 4);
 }
 
 void init_graphics() {
@@ -42,7 +57,7 @@ void init_graphics() {
 
         compute_cycle();
 
-        if (frame_depth == 228*262) draw_frame();
+        if (y_pos == 262) draw_frame();
     }
 
     end_sdl();
@@ -52,11 +67,13 @@ void start_sdl() {
     SDL_Init( SDL_INIT_VIDEO );
     tv = SDL_CreateWindow("atari", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     ren = SDL_CreateRenderer(tv, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-    surf = SDL_LoadBMP("zilker.bmp");
+    tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 640, 384);
+
 }
 
 void end_sdl() {
-    SDL_FreeSurface(surf);
+    delete[] pixels;
+    SDL_DestroyTexture(tex);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(tv);
     SDL_Quit();
